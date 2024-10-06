35b621318562a0a1e5086be52428da198d2b7bd5
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-10-06 08:21:20 +0800

    PHP Print Statements.

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php // Beginning angle bracket
+
+    echo"Fun fact: PHP stands for Personal Home Page<br><br>";
+    
+    echo"PHP also now stands for:<br><br>";
+    echo"P = PHP<br>";
+    echo"H = Hypertext<br>";
+    echo"P = Preprocessor<br><br>";
+
+    echo"This is a print statement with \"DOUBLE QUOTES.\"<br><br>";
+    echo'This is print statement with \'SINGLE QUOTE.\'';
+
+    // Single-line comment.
+
+    /*
+        Multi-line
+        comment
+    */
+    
+    # Python-like single-line comment.
+
+    // Ending angle bracket
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>PHP Print Statement</title>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="Practicing PHP">
+    <meta name="keywords" content="PHP, HTML">
+    <meta name="author" content="Kenzo">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--meta http-equiv="refresh" content="60"-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+</head>
+<body>
+    <br><br><hr><br>
+    <button>Just a button with no purpose.</button>
+    <p>HTML can also be coded inside a PHP file.</p>
+</body>
+</html>
