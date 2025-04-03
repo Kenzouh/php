da489480a8b544ffc1bfe9192163195454256039
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-04-03 16:15:18 +0800

    feat: add products page

---
+++ index19_products.php
@@ -0,0 +1,23 @@
+<?php
+    include("./components/index19_header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    include("./components/index19_head.html");
+?>
+
+
+<body style="text-align: center; padding: 0px; margin: 0px;">
+
+    <h1 style="font-size: 80px; padding: 20px;">This is the Product Page! :D</h1><br/>
+    
+    <h2 style="font-size: 60px; padding: 20px;">Just a testing page. :&rpar;</h2><br/><br/><br/>
+</body>
+</html>
+
+<?php
+    include("./components/index19_footer.html");
+?>
