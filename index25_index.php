a5a3687bf2b916b9267f3c7878d2608e313a4ca7
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-04-10 09:58:44 +0800

    feat: add home page for MySQL DB connection

---
+++ index25_index.php
@@ -0,0 +1,25 @@
+<?php
+    //! Input this in URL tab: http://localhost/php/index25_index.php
+    include("index25_database.php");
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MySQL Database Connection</title>
+
+    <meta charset="UTF-8"/>
+    <meta name="description" content="Implementing MySQL Database Connection in PHP."/>
+    <meta name="keywords" content="PHP, MySQL, Database Connection, Database, Connection, DB"/>
+    <meta name="author" content="Kenzo"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--meta http-equiv="refresh" content="60"/-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+    <link rel="icon" type="image/jpg" href="img/favicon.jpg"/>
+</head>
+<body>
+    Hi!
+</body>
+</html>
